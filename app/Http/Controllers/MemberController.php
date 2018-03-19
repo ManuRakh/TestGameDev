2db2f333ac52625c5427f97626d274a13664c12b
ManuRakh <manucher5160@gmail.com>
2018-03-19 21:46:22 +0300

    Регистрация пользователя

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Member;
 
 class MemberController extends Controller
 {
@@ -13,4 +14,35 @@ class MemberController extends Controller
     {
         return view('addmember');// идет в Resources/views  и берет содержимое по название.blade.php
     }
+    public function addmemberdo(Request $request)
+    {        $member = new Member;
+
+        $this->validate($request,//валидирование входных данных к требованиям админа
+          [
+              'name'=>'required|max:50',
+              'email'=>'required|max:50',
+              'password'=>'required|max:50',
+              'password'=>'required|min:6',
+              'Phone'=>'required|min:6',
+              'confirmpassword' => 'required|same:password',
+              'email'=>'required|unique:members',
+          ]);        
+
+          $input = (object) $request->all();
+
+          $input->password = md5($input->password);
+          $input->confirmpassword = md5($input->confirmpassword);
+          $data =(array) $input;
+        $member->fill($data);
+        
+       
+  
+    $member->save();
+    //Если все хорошо то создаст success 
+   session([    
+            'data' => 'Successfull registered!',
+            ]); 
+            return redirect()->back();// редирект на страницу регистрации
+         }
+    
 }
--- resources/views/addmember.blade.php
+++ resources/views/addmember.blade.php
@@ -1,1 +1,42 @@
-huymember
+
+<?php
+function success()
+{
+if(session()->has('data'))
+{
+	$value = session('data');
+echo "<h4 id = 'succession'>".($value)."</h4>";
+session()->forget('data');
+
+}
+}
+success();
+?>
+<style>
+#succession
+{
+	color:green;
+}
+</style>
+
+
+<form action = "{{route('addmemberdo')}}" method = "POST">
+{{csrf_field()}}<!--  защита от инъекций, токен -->
+
+<input type = "text" placeholder = "Member's name" name = "name"> <br/>
+<input type = "text" placeholder = "Email" name= "email"><br/>
+<input type = "text" placeholder = "Phone number" name = "Phone"><br/>
+<input type = "password" placeholder = "Password" name= "password"><br/>
+<input type = "password" placeholder = "Confirm Passord" name= "confirmpassword"><br/>
+
+<input type = "hidden" value = "{{csrf_token()}}" name="Uid">
+<input type ="submit" value  ="submit">
+</form>
+
+<!-- Если возникнут ошибки, сработает эррор. Обычно это будут ошибки с вводом данных
+Предпочтительно перевести вывод ошибок на Ajax-е, но в требованиях к прожекту не видел. -->
+    @if(count($errors)>0) 
+@foreach($errors->all() as $error)
+{{$error}}<br/>
+@endforeach
+@endif
